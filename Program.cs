2cb8c2dda7d26936708c9e17deb031d3fa4bdca9
Jasper <jaspernoork@gmail.com>
2018-02-13 09:49:17 +0200

    Data2
    Data

--- Program.cs
+++ Program.cs
@@ -41,45 +41,88 @@ namespace DataCollections
             //    Console.WriteLine(cars[index]);
             //}
 
-            var humans1 = new List<Human>
+            //var humans1 = new List<Human>
+            //{
+            //    new Human(){Name = "Kalle", Age = 26 },
+            //    new Human(){Name = "Jasper", Age = 25 },
+            //    new Human(){Name = "Tom", Age = 24 },
+            //    new Human(){Name = "Jakob", Age = 23 },
+            //    new Human(){Name = "Andris", Age = 22 },
+            //};
+
+            //var human1 = new Human();
+            //human1.Name = "Priit";
+            //human1.Age = 25;
+            //humans1.Add(human1);
+
+            //var human2 = new Human();
+            //Console.WriteLine("Sisestage nimi");
+            //human2.Name = Console.ReadLine();
+            //Console.WriteLine("Sisestage oma vanus");
+            //human2.Name = Console.ReadLine();
+
+            //foreach (var item in humans1)
+            //{
+            //Console.WriteLine(item.Name + " " + item.Age);
+            //}
+            //while(true)
+            //{
+                //var human3 = new Human();
+                //Console.WriteLine("Sisestage nimi?");
+                //human3.Name = Console.ReadLine();
+                //Console.WriteLine("Sisestage vanus?");
+                //human3.Age = Int32.Parse(Console.ReadLine());
+                //humans1.Add(human3);
+
+                //Dictionary
+                //            key      value
+                var openWith = new Dictionary<string, string>();
+                openWith.Add("txt", "notepad.exe");
+                openWith.Add("bmp", "paint.exe");
+                openWith.Add("jpg", "paint.exe");
+                openWith.Add("rtf", "workpad.exe");
+
+            openWith["rtf"] = "winword.exe";
+            openWith["doc"] = "winword.exe";
+
+
+            try
             {
-                new Human(){Name = "Kalle", Age = 26 },
-                new Human(){Name = "Jasper", Age = 25 },
-                new Human(){Name = "Tom", Age = 24 },
-                new Human(){Name = "Jakob", Age = 23 },
-                new Human(){Name = "Andris", Age = 22 },
-            };
-
-            var human1 = new Human();
-            human1.Name = "Priit";
-            human1.Age = 25;
-            humans1.Add(human1);
-
-            var human2 = new Human();
-            Console.WriteLine("Sisestage nimi");
-            human2.Name = Console.ReadLine();
-            Console.WriteLine("Sisestage oma vanus");
-            human2.Name = Console.ReadLine();
-
-            foreach (var item in humans1)
-            {
-            Console.WriteLine(item.Name + " " + item.Age);
+                openWith.Add("txt", "workpad");
             }
-            while(true)
+            catch (ArgumentException ex)
             {
-                var human3 = new Human();
-                Console.WriteLine("Sisestage nimi?");
-                human3.Name = Console.ReadLine();
-                Console.WriteLine("Sisestage vanus?");
-                human3.Age = Int32.Parse(Console.ReadLine());
-                humans1.Add(human3);
-
-                foreach (var item in humans1)
-                {
-                    Console.WriteLine(item.Name + " " + item.Age);
-                }
+                Console.WriteLine("An element with key \"txt\" already exists");
 
             }
+
+            Console.WriteLine("For key = rtf, value = {0}" , openWith["rtf"]);
+            Console.WriteLine("For key = doc, value = {0}", openWith["doc"]);
+            Console.WriteLine("For key = tif, value = {0}", openWith["tif "]);
+            try
+            {
+                Console.WriteLine("For key = tif, value = {0}", openWith["tif"]);
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine("Key = \"tif\" is not found");
+            }
+            string value = "";
+            if(openWith.TryGetValue("tif", out value))
+            {
+                Console.WriteLine("For key = \"tif\", value = {0}." value);
+            }
+            else
+            {
+                Console.WriteLine("Key = \"tif\", is not found");
+            }
+            if
+
+
+            //foreach (var item in humans1)
+            //{
+            //    Console.WriteLine(item.Name + " " + item.Age);
+            //}
         }
     }
 }
